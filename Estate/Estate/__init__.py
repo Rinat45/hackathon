eb63e4140eeda01278e6c1f522b52c1e824f436c
“Rinat <rinat99.galimov@gmail.com>
2020-11-14 15:23:18 +0500

    Добавьте файлы проекта.

---
+++ Estate/Estate/__init__.py
@@ -0,0 +1,78 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+import xml.etree.ElementTree as ET
+
+"""
+The flask application package.
+"""
+POSTGRES = {
+    'user': 'manager',
+    'pw': '1234',
+    'db': 'chatbotsystem',
+    'host': '127.0.0.1',
+    'port': '54',
+}
+
+WEB_conf={
+	'HOST'	:'127.0.0.1',
+	'PORT'	:5000,
+	'CERT'	:'cert.pem',
+	'KEY'	:'key.pem',
+}
+
+def ReadConfigFromXML(nameFileXML):
+	try:
+		tree = ET.parse(nameFileXML)
+		root = tree.getroot()
+		web=root.find('web_server')
+		host=web.find('host').text
+		WEB_conf['HOST']=host
+		port=int(web.find('port').text)
+		WEB_conf['PORT']=port
+		cert=web.find('cert').text
+		WEB_conf['CERT']=cert
+		key=web.find('key').text
+		WEB_conf['KEY']=key
+		web=root.find('postgresql')	
+		POSTGRES['db']=web.find('database').text
+		POSTGRES['pw']=web.find('password').text
+		POSTGRES['user']=web.find('user').text
+		POSTGRES['host']=web.find('host').text
+		POSTGRES['port']=web.find('port').text	
+	except Exception as e:
+		print('Error read config:'+str(e))
+		return False
+	return True
+
+
+
+
+
+
+from flask import Flask
+
+ReadConfigFromXML('config.xml')
+
+print('Postgresql=',POSTGRES)
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+from .models import User
+@login_manager.user_loader
+def load_user(user_id):
+        	# since the user_id is just the primary key of our user table, use it in the query for the user
+	        return User.query.get(int(user_id))
+
+import Estate.views
+import Estate.auth
---
+++ Estate/Estate/models.py
@@ -0,0 +1,58 @@
+from flask_login import UserMixin
+from Estate import db
+import datetime
+
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    org = db.Column(db.String(1000))
+    tel = db.Column(db.String(30))
+    role = db.Column(db.SmallInteger, default = ROLE_USER)
+    def encode_auth_token(self):
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=10, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': self.id,
+                'email':self.email
+            }
+            return jwt.encode(
+                payload,
+                TOKEN.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+    @staticmethod
+    def decode_auth_token(auth_token):
+        payload=None
+        try:
+            print('auth_token=',auth_token)
+            payload = jwt.decode(auth_token, TOKEN.get('SECRET_KEY'))
+            print('payload=',payload)
+            return 0, payload
+        except jwt.ExpiredSignatureError:
+            print('expired')	
+            return -1, payload
+        except jwt.InvalidTokenError:
+            print('invalid')	
+            return -2, payload
+
+class estates(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id_org=db.Column(db.Integer)
+    name_estate=db.Column(db.String(500))
+    addr = db.Column(db.String(1000))
+    area = db.Column(db.Float)
+    kadastr = db.Column(db.String(200))
+    description = db.Column(db.String(1000))
+    contact=db.Column(db.String(500))
+    img=db.Column(db.LargeBinary)
+
+    
